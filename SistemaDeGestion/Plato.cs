6278d048e900ecd4dc0d34b09c99808c42dc1a92
BraianBarrionuevo <braianbarrionuevo@alu.frt.utn.edu.ar>
2026-05-04 21:41:40 -0300

    Seguir en Casa

--- SistemaDeGestion/Plato.cs
+++ SistemaDeGestion/Plato.cs
@@ -2,15 +2,40 @@ namespace Restaurante
 {
     public abstract class Plato
     {
-        public string Nombre { get; set; }
-        public decimal Precio { get; set; }
 
-        public Plato(string nombre, decimal precio)
+        private string _nombre { get; }
+        private string _idPlato { get; }
+
+        private string _categoria;
+        public string Categoria
         {
-            Nombre = nombre;
-            Precio = precio;
+
+            get => _categoria;
+
+            set
+            {
+
+                if (_categoria == null) throw new ArgumentNullException("La categoria no puede ser null");
+    
+
+            _categoria = value;
+
+            }
+
         }
 
-        public abstract void Preparar();
+
+        public int Anio {get; set;}
+
+        public Plato(string nombre, string idPlato, string categoria, int anio)
+        {
+            _nombre = nombre;
+            _idPlato = idPlato;
+            _categoria = categoria;
+            Anio = anio;
+        }
+
+    
+    
     }
 }
